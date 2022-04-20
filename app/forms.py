14eef2f1f0651e85f7594df8a508bcaf0c1085a2
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-04-20 15:07:46 +0300

    add wtforms

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class Regform(FlaskForm):
-    first_name = StringField("Имя",validators=[DataRequired()])
-    last_name = StringField("Фамилия",validators=[DataRequired()])
-    fathers_name = StringField("Отчество",validators=[DataRequired()])
-    date_of_brith = StringField("Дата рождения",validators=[DataRequired()])
-    location = SelectField("Место рождения",validators=[DataRequired()])
-    submit = SubmitField('Отправить')
+    first_name = StringField(label="Имя",validators=[DataRequired()])
+    last_name = StringField(label="Фамилия",validators=[DataRequired()])
+    fathers_name = StringField(label="Отчество",validators=[DataRequired()])
+    date_of_brith = StringField(label="Дата рождения",validators=[DataRequired()])
+    location = SelectField(label="Место рождения",choices=[(1,"Москва"),(2,"МО"), (3, "Россия")], validators=[DataRequired()])
+    submit = SubmitField(label='Отправить')
 
 
